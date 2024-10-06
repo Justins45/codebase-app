1ea387d1a632b5a0b8a20e88361022e0cf3300b9
Justin Shaw <shaw.justin@shaw.ca>
2024-10-06 00:00:09 -0600

    add starttying layout of side nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
+import "./assets/ResetCss.css"
 import "./App.css";
 
 function App() {
@@ -13,23 +14,49 @@ function App() {
   }
 
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      
+      {/* Nav Start */}
+      <div className="nav-bar">
+        {/* Nav Open and Close */}
+        <div className="sidebar-toggle">
+          <p>&larr;</p>
+        </div> 
+        <div className="nav-group">
+          <div>
+            <div className="nav-group-heading">
+              <p className="nav-group-title">Projects</p>
+              <div className="nav-group-chevron">
+                ^
+              </div>
+            </div>
+            <div className="divider" />
+          </div>
+          <div className="nav-items">
+            <p>+ Add New Project</p>
+          </div>
+        </div>
+        <div className="nav-group">
+          <div>
+          <div className="nav-group-heading">
+              <p className="nav-group-title">Todo</p>
+              <div className="nav-group-chevron">
+                ^
+              </div>
+            </div>
+            <div className="divider" />
+          </div>
+          <div className="nav-items">
+            <p>+ Add Todo items</p>
+          </div>
+        </div>
       </div>
+      {/* Nav End */}
+    </div>
 
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
+    // Tauri -> Rust function talking
+    /* 
       <form
         className="row"
         onSubmit={(e) => {
@@ -46,7 +73,7 @@ function App() {
       </form>
 
       <p>{greetMsg}</p>
-    </div>
+     */
   );
 }
 
